907cdf94e873fb97ccc91a34b4fcc0537b61c9f0
Tais Reis <tatareis15@gotmail.com>
2019-04-02 20:43:51 -0300

    [ FEATURE ] Criação do menu da aplicação.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,9 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Header from '../Header';
 
 class App extends Component {
     render() {
         return (
-            <h1>App</h1>
+            <Fragment>
+                <Header />
+            </Fragment>
         );
     }
 }
---
+++ src/components/Header/index.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import Dashboard from '../Dashboard';
+import List from '../List';
+import Register from '../Register';
+
+class Header extends Component {
+    render() {
+        return (
+            <header>
+                <Router>
+                    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                        <div className="container">
+                            <span className="navbar-brand mb-0 h1">Quanto Gastei?</span>
+
+                            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#mainNavbar" aria-controls="mainNavbar" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+
+                            <div className="collapse navbar-collapse" id="mainNavbar">
+                                <ul className="navbar-nav">
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="/">Dashboard</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="/list">Minhas Transações</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link" href="/register">Adicionar Transação</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+
+                    <Route path="/" exact component={Dashboard} />
+                    <Route path="/list" exact component={List} />
+                    <Route path="/register" exact component={Register} />
+                </Router>
+            </header>
+        );
+    }
+}
+
+export default Header;
