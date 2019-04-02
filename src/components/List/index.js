54cb688d3aadc8276bb48ee16dbb38e5f935ede5
Tais Reis <tatareis15@gotmail.com>
2019-04-02 18:04:19 -0300

    [ META ] Estrutura de pastas criada.

---
+++ src/components/List/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+    render() {
+        return (
+            <h1>Listar</h1>
+        );
+    }
+}
+
+export default List;
---
+++ src/components/Transaction/index.js
@@ -0,0 +1,11 @@
+import  React, { Component } from 'react';
+
+class Transaction extends Component {
+    render() {
+        return (
+            <h1>Transação</h1>
+        );
+    }
+}
+
+export default Transaction;
