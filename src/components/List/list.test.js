f6fdd6c614675eff31f9f4cba0b126e8c7d78eb7
Tais Reis <tatareis15@gotmail.com>
2019-04-04 22:48:33 -0300

    [ REFAC ] Alteração dos testes para testar as novas alterações.

--- src/components/List/list.test.js
+++ src/components/List/list.test.js
@@ -2,8 +2,22 @@ import React from 'react';
 import List from '../List';
 import { mount } from 'enzyme';
 import toJson from 'enzyme-to-json';
+import ls from 'local-storage';
+
+it('Deve renderizar uma mensagem de nenhuma transação cadastrada', () => {
+    const list = mount(<List />);
+
+    expect(toJson(list)).toMatchSnapshot();
+});
 
 it('Deve renderizar uma listagem com as transações adicionadas', () => {
+    const transactions = [
+        ['R$ 230,50', 'Tênis'],
+        ['R$ 230,50', 'Tênis'],
+    ];
+
+    ls.set('transactions', transactions);
+
     const list = mount(<List />);
 
     expect(toJson(list)).toMatchSnapshot();
